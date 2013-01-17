87cb9c7b788436350bda2be152823560de1bc621
Eunchong Yu <kroisse@gmail.com>
2013-01-17 19:02:52 +0900

    Add setup.py

--- repeat.py
+++ repeat.py
@@ -7,11 +7,17 @@ parser = argparse.ArgumentParser(description='Repeat job N times per S seconds.'
 parser.add_argument('-n', type=int, default=10, help='Will repeat N times. (default=10)')
 parser.add_argument('-s', type=int, default=1, help='Will repeat per S seconds. (default=1)')
 parser.add_argument('cmd', nargs=argparse.REMAINDER, help='Command to be repeated.')
-args = parser.parse_args()
 
-if len(args.cmd):
-    for _ in xrange( args.n ):
-        Popen( args.cmd )
-        sleep( args.s )
-else:
-    print 'Nothing to run. Stopped.'
+
+def main():
+    args = parser.parse_args()
+    if len(args.cmd):
+        for _ in xrange( args.n ):
+            Popen( args.cmd )
+            sleep( args.s )
+    else:
+        print 'Nothing to run. Stopped.'
+
+
+if __name__ == '__main__':
+    main()
