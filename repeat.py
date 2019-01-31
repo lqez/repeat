29a0adcb04e84ea690023875dc3bf6924c893799
Park Hyunwoo <ez.amiryo@gmail.com>
2019-01-31 17:48:53 +0900

    Update help

--- repeat.py
+++ repeat.py
@@ -12,9 +12,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Repeat job N times per S seconds.')
-parser.add_argument('-n', type=int, default=10, help='Will repeat N times. (default=10, infinite=0)')
-parser.add_argument('-s', type=int, default=1, help='Will repeat per S seconds. (default=1)')
-parser.add_argument('-v', help='Print datetime on every run. (default=False)', action='store_true')
+parser.add_argument('-n', type=int, default=10, help='repeat N times. (default=10, infinite=0)')
+parser.add_argument('-s', type=int, default=1, help='repeat every S seconds. (default=1)')
+parser.add_argument('-v', help='print datetime on every run. (default=False)', action='store_true')
 parser.add_argument('cmd', nargs=argparse.REMAINDER, help='Command to be repeated.')
 
 
