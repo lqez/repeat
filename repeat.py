71546fcade25bff89a8c2d65362eea4ea2a35694
Park Hyunwoo <ez.amiryo@gmail.com>
2019-01-31 17:43:58 +0900

    Improved by ptimer and support for Python3

--- repeat.py
+++ repeat.py
@@ -5,28 +5,31 @@
 # Repeat command N times every S seconds.
 #
 
-import argparse
-from time import sleep
+from datetime import datetime
+from ptimer import repeat
 from subprocess import Popen
+import argparse
+
 
 parser = argparse.ArgumentParser(description='Repeat job N times per S seconds.')
-parser.add_argument('-n', type=int, default=10, help='Will repeat N times. (default=10)')
+parser.add_argument('-n', type=int, default=10, help='Will repeat N times. (default=10, infinite=0)')
 parser.add_argument('-s', type=int, default=1, help='Will repeat per S seconds. (default=1)')
+parser.add_argument('-v', help='Print datetime on every run. (default=False)', action='store_true')
 parser.add_argument('cmd', nargs=argparse.REMAINDER, help='Command to be repeated.')
-
-
-def repeat(cmds, n=10, s=1):
-    for _ in xrange(n):
-        Popen(cmds)
-        sleep(s)
 
 
 def main():
     args = parser.parse_args()
+
+    def run():
+        if args.v:
+            print(datetime.now())
+        Popen(args.cmd)
+
     if len(args.cmd):
-        repeat(args.cmd, args.n, args.s)
+        repeat(run, args.n if args.n > 0 else -1, args.s)
     else:
-        print 'Nothing to run. Stopped.'
+        print('Nothing to run. Stopped.')
 
 
 if __name__ == '__main__':
